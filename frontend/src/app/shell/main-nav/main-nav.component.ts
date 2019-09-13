f4fcfee5545fcd87e5865ddeb125ed7181177ca3
balintcsaszar <balint.csaszar@dpc.hu>
2019-09-13 21:06:46 +0200

    fix: sidenav closing on menu click

--- frontend/src/app/shell/main-nav/main-nav.component.ts
+++ frontend/src/app/shell/main-nav/main-nav.component.ts
@@ -1,17 +1,21 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component, Input } from '@angular/core';
-import { Observable } from 'rxjs';
-import { debounceTime, map, share } from 'rxjs/operators';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Event as NavigationEvent, NavigationEnd, Router } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { debounceTime, filter, map, share } from 'rxjs/operators';
 
 import { MenuItem } from '../types/menu-item.interface';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'pi-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit {
   @Input() menuItems: MenuItem[] = [];
+
+  @ViewChild(MatSidenav, { static: true }) drawer!: MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     debounceTime(1),
@@ -19,5 +23,18 @@ export class MainNavComponent {
     share()
   );
 
-  constructor(private readonly breakpointObserver: BreakpointObserver) {}
+  navigationEnd$: Observable<NavigationEvent> = this.router.events.pipe(filter((event: NavigationEvent) => event instanceof NavigationEnd));
+
+  constructor(private readonly breakpointObserver: BreakpointObserver, private readonly router: Router) {}
+
+  ngOnInit(): void {
+    combineLatest([this.isHandset$, this.navigationEnd$])
+      .pipe(
+        map(([isHandset, navigationEnd]: [boolean, NavigationEvent]) => isHandset),
+        filter((isHandset) => isHandset)
+      )
+      .subscribe(() => {
+        this.drawer.close();
+      });
+  }
 }
