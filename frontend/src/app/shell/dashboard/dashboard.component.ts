9572b6a270450d04674bcdfe77fbbbd9cf8739ba
balintcsaszar <balint.csaszar@dpc.hu>
2019-11-02 11:46:29 +0100

    feat: pdf generator demo on dashboard

--- frontend/src/app/shell/dashboard/dashboard.component.ts
+++ frontend/src/app/shell/dashboard/dashboard.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'pi-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  generatePdf(): void {
+    const pdf = new jsPDF();
+    const img = new Image();
+    img.src = 'assets/kelen-logo.jpg';
+
+    pdf.addImage(img, 'JPEG', 170, 15, 20, 26);
+
+    pdf.save('x.pdf');
+  }
+}
