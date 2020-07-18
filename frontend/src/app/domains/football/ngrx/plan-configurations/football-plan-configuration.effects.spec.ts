4433830b7cea69528385cee9bd80877e57eb6c96
csaszika <csaszika4@gmail.com>
2020-07-18 19:38:31 +0200

    test: football config effect
    chore: skipLibCheck with typescript by tests

--- frontend/src/app/domains/football/ngrx/plan-configurations/football-plan-configuration.effects.spec.ts
+++ frontend/src/app/domains/football/ngrx/plan-configurations/football-plan-configuration.effects.spec.ts
@@ -1,31 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
+import { cold } from 'jasmine-marbles';
+import { Observable, of } from 'rxjs';
 
 import { PlanConfigurationService } from '../../../../shared/services/plan-configuration/plan-configuration.service';
+import { getFootballConfigurations, loadFootballConfigurations } from './football-plan-configuration.actions';
 import { FootballPlanConfigurationEffects } from './football-plan-configuration.effects';
-import createSpy = jasmine.createSpy;
 
 describe('FootballPlanConfigurationEffects', () => {
   let actions$: Observable<any>;
   let effects: FootballPlanConfigurationEffects;
-  const mockPlanConfigurationservice = {
-    getPlanConfigurationByFeature: createSpy('getPlanConfigurationByFeature').and.callThrough(),
+
+  const mockPlanConfigurationService = {
+    getPlanConfigurationByFeature$: jasmine.createSpy('getPlanConfigurationByFeature$'),
   };
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       providers: [
         FootballPlanConfigurationEffects,
+        { provide: PlanConfigurationService, useValue: mockPlanConfigurationService },
         provideMockActions(() => actions$),
-        { provide: PlanConfigurationService, mockValue: mockPlanConfigurationservice },
       ],
     });
 
-    effects = TestBed.get<FootballPlanConfigurationEffects>(FootballPlanConfigurationEffects);
-  });
+    effects = TestBed.inject(FootballPlanConfigurationEffects);
+  }));
+
+  const configurationData = {
+    ageClasses: ['U7', 'U8', 'U9'],
+    // tslint:disable-next-line:no-magic-numbers
+    levels: [1, 2, 3]
+  };
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  it('should load football configuration', () => {
+    const action = getFootballConfigurations;
+    const result = loadFootballConfigurations({ configuration: configurationData });
+
+    actions$ = of(action);
+    const response$ = cold('-a|', { a: { data: () => configurationData} });
+    const expected$ = cold('-b|', { b: result });
+
+    mockPlanConfigurationService.getPlanConfigurationByFeature$.and.returnValue(response$);
+
+    expect(effects.loadFootballConfiguration$).toBeObservable(expected$);
   });
 });
