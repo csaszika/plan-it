7658f42bc45372080c55c4f9c66b98f44081f3dd
csaszika <csaszika4@gmail.com>
2021-03-22 15:36:22 +0100

    fix(ngrx store football): exported members

--- libs/ngrx-store/football/src/index.ts
+++ libs/ngrx-store/football/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/football/plan-configurations/football-plan-configuration.selectors';
-export * from './lib/football/plan-configurations/football-plan-configuration.reducer';
-export * from './lib/football/football.state';
+export { FootballPlanConfigurationState } from './lib/football/plan-configurations/football-plan-configuration.reducer';
+export { FootballState } from './lib/football/football.state';
 export * from './lib/football.store.module';
